60151220df6c1f3e2617e1b5f5f1f3b52a56a765
Imfortes <imfortes@list.ru>
2025-12-21 19:10:59 +0300

    test

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -9,3 +9,20 @@ describe("Пример теста", () => {
         expect(demo(str)).toBe(expected);
     });
 });
+
+
+import Game from './classes/Game.js';
+
+describe('Game', () => {
+  test('should create board with 16 holes', () => {
+    const game = new Game();
+    game.startGame();
+    const holes = document.querySelectorAll('.hole');
+    expect(holes.length).toBe(16);
+  });
+
+  test('should spawn creature in random hole', () => {
+    const game = new Game();
+    game.startGame();
+  });
+});
